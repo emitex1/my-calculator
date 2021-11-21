82374a1154aa6e1eeae7b5db452d2eba2777e279
EmIT <emad.armoun@gmail.com>
2021-11-21 17:38:29 +0330

    style: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import MyCalculator from './MyCalculator';
+import { GlobalStyles } from 'twin.macro'
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <MyCalculator />
   </React.StrictMode>,
   document.getElementById('root')
