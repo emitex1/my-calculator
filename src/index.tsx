067d164cde3f4f129edb0d50807a4cb612b35f64
EmIT <emad.armoun@gmail.com>
2021-11-21 16:04:43 +0330

    feat: add calculator to main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyCalculator from './MyCalculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyCalculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
