90c994fd67a058909fd74516b32fa24c97e1adfa
EmIT <emad.armoun@gmail.com>
2021-11-26 07:18:51 +0330

    style: customize preview themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { themes } from '@storybook/theming';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +7,29 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  docs: {
+    theme: themes.dark,
+  },
+  backgrounds: {
+    default: 'white',
+    values: [
+      {
+        name: 'white',
+        value: '#FFFFFF',
+      },
+      {
+        name: 'black',
+        value: '#000000',
+      },
+      {
+        name: 'dark',
+        value: '#2f2f2f',
+      },
+      {
+        name: 'gray',
+        value: '#999999',
+      },
+    ],
   },
 }
