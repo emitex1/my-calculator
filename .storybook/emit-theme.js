9308ebb9026eff2e37cbc1befa82a505554e5a5e
EmIT <emad.armoun@gmail.com>
2021-11-26 07:19:14 +0330

    style: create custom theme for storybook

---
+++ .storybook/emit-theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  colorSecondary: '#cc00ff',
+
+  brandTitle: 'EmIT Storybook',
+  brandUrl: 'http://www.Armoun.com',
+  brandImage: 'http://www.armoun.com/wp-content/uploads/2021/11/emad-logo-bp.png',
+});
