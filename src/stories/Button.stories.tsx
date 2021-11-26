d419808aba25b52a6ffd73aca344db20e700daa2
EmIT <emad.armoun@gmail.com>
2021-11-26 23:40:45 +0330

    docs: update btn stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-//import ButtonDocumentations from './Button.docs.mdx';
+import ButtonDocumentations from './Button.docs.mdx';
 
 import Button from '../components/Button/Button';
 
@@ -10,13 +10,17 @@ export default {
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    //backgroundColor: { control: 'color' },
+    count: {
+      // control: "number",
+      // control: "range",
+      control: { type: 'range', min: 0, max: 9, step: 1 }
+    },
   },
   parameters:{
     layout:'centered',
-    /*docs: {
+    docs: {
       page: ButtonDocumentations,
-    }*/
+    }
   },
 } as ComponentMeta<typeof Button>;
 
@@ -46,4 +50,45 @@ export const Reset = Template.bind({});
 Reset.args = {
   label: 'C',
   type: 'reset',
+};
+
+export const All = () => {
+  return (
+    <>
+      <Button type="number" label="1" onClick={() => {}} />
+      <Button type="number" label="2" onClick={() => {}} />
+      <Button type="number" label="3" onClick={() => {}} />
+      <Button type="number" label="4" onClick={() => {}} />
+
+      <br/>
+
+      <Button type="operator" label="+" onClick={() => {}} />
+      <Button type="operator" label="-" onClick={() => {}} />
+      <Button type="operator" label="*" onClick={() => {}} />
+      <Button type="operator" label="/" onClick={() => {}} />
+
+      <br />
+
+      <Button type="equal" label="=" onClick={() => {}} />
+      <Button type="reset" label="C" onClick={() => {}} />
+    </>
+  )
+}
+
+const RangeTemplate: ComponentStory<JSX.Element[]> = (args: any) => {
+  const count = Math.min(9, args.count);
+
+  const arr = [];
+  for(let i=0; i<=count; ++i)
+    arr.push(i.toString());
+
+  const list = arr.map( (n: string) => (
+    <Button type="number" label={n} onClick={()=>{}} />
+  ));
+  return list;
+}
+
+export const Range = RangeTemplate.bind({});
+Range.args = {
+  count: '5'
 };
