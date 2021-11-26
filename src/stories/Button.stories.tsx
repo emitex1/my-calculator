cb1f5361e92102b766516067e1e5e2a5a31d1be0
EmIT <emad.armoun@gmail.com>
2021-11-26 21:54:48 +0330

    fix: replace btn cmp in stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import ButtonDocumentations from './Button.docs.mdx';
+//import ButtonDocumentations from './Button.docs.mdx';
 
-import { Button } from './Button';
+import Button from '../components/Button/Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -10,46 +10,40 @@ export default {
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    //backgroundColor: { control: 'color' },
   },
   parameters:{
     layout:'centered',
-    docs: {
+    /*docs: {
       page: ButtonDocumentations,
-    }
+    }*/
   },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const Number = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+Number.args = {
+  label: '1',
+  type: 'number'
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Operator = Template.bind({});
+Operator.args = {
+  label: '+',
+  type: 'operator'
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
+export const Equal = Template.bind({});
+Equal.args = {
+  label: '=',
+  type: 'equal'
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
-
-export const EmadStory = Template.bind({});
-EmadStory.args = {
-  label: "Emad Button",
-  backgroundColor: "#FF00F0",
-  size: 'large'
+export const Reset = Template.bind({});
+Reset.args = {
+  label: 'C',
+  type: 'reset',
 };
